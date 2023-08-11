b84faff4db5ba266d842d4d76a224ce5e829c92a
Shaswat Paudel <shaswat@ezops.ca>
2023-08-11 09:59:07 -0600

    fix

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,11 +4,17 @@ import { AuthDto } from "./dto";
 import * as argon from 'argon2'
 import { error } from "console";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class AuthService {
 
-  constructor(private prisma: PrismaService) { }
+  constructor(
+    private prisma: PrismaService,
+    private jtw: JwtService,
+    private config: ConfigService
+  ) { }
 
   async signup(dto: AuthDto) {
     try {
@@ -50,19 +56,30 @@ export class AuthService {
   async signin(dto: AuthDto) {
 
     const user = await this.prisma.users.findUnique({
-      where : {
+      where: {
         email: dto.email
       }
     })
 
-    if(!user) throw new ForbiddenException('Credential incorrect')
+    if (!user) throw new ForbiddenException('Credential incorrect')
 
     const pwMatch = await argon.verify(user.hash, dto.password)
 
-    if(!pwMatch)  throw new ForbiddenException('Credential incorrect')
+    if (!pwMatch) throw new ForbiddenException('Credential incorrect')
 
-    // Send back the user info
-    delete user.hash
-    return user
+    return this.signToken(user.id, user.email)
+  }
+
+  // We do not need await here because it returns a promise
+  signToken(userId: number, email: string): Promise<string> {
+    const payload = {
+      sub: userId,
+      email
+    }
+
+    const secret =  this.config.get('JWT_SECRET')
+
+    // Return tokens
+    return this.jtw.signAsync(payload, { expiresIn : '15m', secret }) 
   }
 }
